870d82f8e870dd5ab5dc61da8e8f2cbd8be4f8cb
Ashna26-Mittal <ashna.frnds@gmail.com>
2023-11-12 09:00:41 +0530

    updated main code

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+# Importing dependencies
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+ 
+# Including & Reading the CSV file:
+df = pd.read_csv("tumor_data.csv")
+print(df.head())
